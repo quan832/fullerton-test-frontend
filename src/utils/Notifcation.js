f89c0111ee19a234d57a29db57d57701ccfae749
quantran238 <85110073+quantran238@users.noreply.github.com>
2021-11-28 21:29:58 +0700

    fix notification error

--- src/utils/Notifcation.js
+++ src/utils/Notifcation.js
@@ -5,10 +5,10 @@ import { message } from 'antd';
 // -> we will display try again for user
 export function getError(err) {
   let error = 'Error! Please try again';
-  if (err.response && err.response.data.error) {
-    const firstError = err.response.data.error;
-    if (firstError.message) {
-      error = firstError.message;
+  if (err.response && err.response.data.errors) {
+    const firstError = err.response.data.errors[0];
+    if (firstError.msg) {
+      error = firstError.msg;
     }
   } else if (err.message || err) {
     error = err.message || err;
